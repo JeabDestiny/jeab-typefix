0424e01319ebf6cdeeef945f6268132b19b14018
JeabJeab <95631926+JeabDestiny@users.noreply.github.com>
2026-07-08 07:27:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const m={'l':'ส',';':'ว','y':'ั','f':'ด','u':'ี'},r={};for(let k in m)r[m[k]]=k;in.oninput=()=>{let t=in.value,th=/[ก-๙]/.test(t);out.value=[...t].map(c=>th?(r[c]||c):(m[c]||c)).join('')}
+const m={'l':'ส',';':'ว','y':'ั','f':'ด','u':'ี'},r={};
+for(let k in m) r[m[k]]=k;
+
+const inputBox=document.getElementById('in');
+const outputBox=document.getElementById('out');
+
+inputBox.oninput=()=>{
+  let t=inputBox.value, th=/[ก-๙]/.test(t);
+  outputBox.value=[...t].map(c=>th?(r[c]||c):(m[c]||c)).join('');
+};
